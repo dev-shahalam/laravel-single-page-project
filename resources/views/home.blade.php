a28f9a2651d36e93951e23bbfd7a870b9c215ed0
Md Shah Alam <mdshahalam8283@gmal.com>
2024-06-30 02:27:27 +0600

    Laravel single page development

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+@section('content')
+    @include('components.hero')
+    @include('components.about')
+    @include('components.pricing')
+    @include('components.team')
+    @include('components.newslatter')
+@endsection
